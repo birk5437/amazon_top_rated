badbcb8e10edc3953fa92ad56dfe049f138de495
Burke Ramsey <birk5437@gmail.com>
2015-03-13 03:06:30 -0400

    bugfix: sort_order in item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -120,6 +120,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:url, :title, :description, :price, :thumbnail)
+      params.require(:item).permit(:url, :title, :description, :price, :thumbnail, :sort_order)
     end
 end
